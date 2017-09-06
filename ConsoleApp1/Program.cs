ac55c4355d260358cb125ca5865a0fe36fff4642
Udo <zverevi007@mail.ru>
2017-09-06 19:26:55 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        private static readonly ulong m = Int64.MaxValue;
+        private static readonly ulong a = 1103515245;
+        private static readonly ulong b = 12345;
+        private static ulong current = 2;
+
+        private static double next()
+        {
+            return Convert.ToDouble(current = ((a * current + b) % m)) / Int64.MaxValue;
+        }
+
+        public static void Main(string[] args)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"log.txt"))
+
+                for (int i = 0; i < 1000; i++)
+            {
+                string line = String.Format("{0:0.00000000000}", next());
+                Console.WriteLine(line);
+                file.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RandomGenerator")]
+[assembly: AssemblyDescription("Generates 100000 numbers and stores them in a file")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RandomGen")]
+[assembly: AssemblyCopyright("Copyright © UdoChudo  2017")]
+[assembly: AssemblyTrademark("Udo")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("287a6bd4-7fe1-477c-b36b-fc50631fb7f9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
