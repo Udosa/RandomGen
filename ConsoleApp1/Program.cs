1020e1b63680e399db1a053c9a82eec8b054882c
Udo <zverevi007@mail.ru>
2017-09-07 17:41:14 +0500

    Add some trash function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
         private static readonly ulong a = 1103515245;
         private static readonly ulong b = 12345;
         private static ulong current = 2;
-
+        
         private static double next()
         {
             return Convert.ToDouble(current = ((a * current + b) % m)) / Int64.MaxValue;
@@ -17,14 +17,19 @@ namespace ConsoleApp1
 
         public static void Main(string[] args)
         {
+
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"log.txt"))
 
-                for (int i = 0; i < 1000; i++)
+                for (int i = 0; i < 100000; i++)
             {
                 string line = String.Format("{0:0.00000000000}", next());
-                Console.WriteLine(line);
-                file.WriteLine(line);
-            }
+                    //int x = int.Parse(line);
+                    //x = x + x ;
+                    Console.WriteLine(line);
+                    file.WriteLine(line);
+            
+                }
+
             Console.ReadLine();
         }
     }
